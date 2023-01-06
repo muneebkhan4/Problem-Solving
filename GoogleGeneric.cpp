1cb6ad8a05357486d3ea92050db94fffc80dd162
Muhammad Muneeb Ur Rehman <76596205+muneebkhan4@users.noreply.github.com>
2023-01-06 19:19:53 +0500

    Create GoogleGeneric.cpp
    It is common input and output code helpful in solving Google Problems Fast.

---
+++ GoogleGeneric.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int tests=0;
+    cin>>tests; // number of test cases
+    
+    for(int i=0;i<tests;i++){
+        // input stream
+        int n=0,k=0;
+        string str;
+        cin>> n;
+        cin>> k;
+        cin>>str;
+        
+        // algo
+        int res =0;  
+      
+        // output stream
+        cout<<"Case #"<<i+1<<": "<<res<<endl;
+    }
+
+    return 0;
+}

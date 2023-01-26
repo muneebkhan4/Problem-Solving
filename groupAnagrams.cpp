a41ba4a148e04255820cc986084a934649286932
Muhammad Muneeb Ur Rehman <76596205+muneebkhan4@users.noreply.github.com>
2023-01-27 02:14:51 +0500

    Update groupAnagrams.cpp
    Best Solution added...

--- groupAnagrams.cpp
+++ groupAnagrams.cpp
@@ -35,3 +35,45 @@ public:
     }
 };
 // 11 14  11 42
+
+
+
+
+// Best Solution
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+       
+       vector<vector<string>>res;
+        
+       unordered_map<string, vector<string>>mp;
+        
+         /*
+                Consider example 1 : strs = ["eat","tea","tan","ate","nat","bat"]
+                
+                After the below opeartion of for loop map will contain
+                
+                aet -- eat, tea, ate
+                ant -- tan, nat
+                abt -- bat
+        */
+        
+        for(int i = 0 ; i < strs.size() ; i++)
+        {
+            string s = strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(s);
+        }
+        
+        //now simply put the elements  of second column of map in ans
+        
+        for(auto i : mp)
+        {
+            res.push_back(i.second);
+        }
+
+        return res;
+    }
+};
+// 11 14  11 42

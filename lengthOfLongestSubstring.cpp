75c168bb368046e4ab166b17e41a4cc86456f658
Muhammad Muneeb Ur Rehman <76596205+muneebkhan4@users.noreply.github.com>
2023-08-04 18:07:26 +0500

    Create lengthOfLongestSubstring.cpp
    Given a string s, find the length of the longest 
    substring without repeating characters.

---
+++ lengthOfLongestSubstring.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+         int n = s.length();
+        unordered_set<char> charSet;
+        int maxLength = 0, left = 0, right = 0;
+        
+        while (right < n) {
+            if (charSet.find(s[right]) == charSet.end()) {
+                charSet.insert(s[right]);
+                maxLength = max(maxLength, right - left + 1);
+                right++;
+            } else {
+                charSet.erase(s[left]);
+                left++;
+            }
+        }
+        
+        return maxLength;
+    }
+};

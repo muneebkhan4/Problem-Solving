5e51343e866fa05cf0c7ff5146b3e985722a4f2c
Muhammad Muneeb Ur Rehman <76596205+muneebkhan4@users.noreply.github.com>
2023-08-07 15:53:34 +0500

    Update Minimum_XOR_Sum_of_Two_Arrays.cpp
    You are given two integer arrays nums1 and nums2 of length n.
    
    The XOR sum of the two integer arrays is (nums1[0] XOR nums2[0]) + (nums1[1] XOR nums2[1]) + ... + (nums1[n - 1] XOR nums2[n - 1]) (0-indexed).
    
    For example, the XOR sum of [1,2,3] and [3,2,1] is equal to (1 XOR 3) + (2 XOR 2) + (3 XOR 1) = 2 + 0 + 2 = 4.
    Rearrange the elements of nums2 such that the resulting XOR sum is minimized.
    
    Return the XOR sum after the rearrangement.
    
     
    
    Example 1:
    
    Input: nums1 = [1,2], nums2 = [2,3]
    Output: 2
    Explanation: Rearrange nums2 so that it becomes [3,2].
    The XOR sum is (1 XOR 3) + (2 XOR 2) = 2 + 0 = 2.
    Example 2:
    
    Input: nums1 = [1,0,3], nums2 = [5,3,4]
    Output: 8
    Explanation: Rearrange nums2 so that it becomes [5,4,3]. 
    The XOR sum is (1 XOR 5) + (0 XOR 4) + (3 XOR 3) = 4 + 4 + 0 = 8.

--- Minimum_XOR_Sum_of_Two_Arrays.cpp
+++ Minimum_XOR_Sum_of_Two_Arrays.cpp
@@ -1,31 +1,42 @@
-
 class Solution {
 public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        vector<int> dp(1 << n, -1);
+        vector<int> dp(1 << n, -1); // Memoization array to store the minimum XOR sum for each state (bitmask).
+
+        // Call the recursive function with the initial state (i=0, mask=0) and return the result.
         return dfs(0, 0, nums1, nums2, dp);
     }
 
     int dfs(int i, int mask, vector<int>& nums1, vector<int>& nums2, vector<int>& dp) {
         int n = nums1.size();
+
+        // Base case: If we have reached the end of the nums1 array, return 0 (no more elements to process).
         if (i == n) {
             return 0;
         }
 
+        // Check if the result for the current state (i, mask) is already computed and stored in the dp array.
         if (dp[mask] != -1) {
             return dp[mask];
         }
 
-        int min_xor = numeric_limits<int>::max();
+        int min_xor = numeric_limits<int>::max(); // Initialize min_xor to maximum value.
+
+        // Try all possible permutations of nums2 elements and calculate the XOR sum at each step.
         for (int j = 0; j < n; j++) {
+            // Check if the jth element of nums2 is not used in the current permutation (mask bit is 0).
             if ((mask & (1 << j)) == 0) {
                 int xor_sum = (nums1[i] ^ nums2[j]) + dfs(i + 1, mask | (1 << j), nums1, nums2, dp);
+                // Update min_xor with the minimum XOR sum among all possible permutations.
                 min_xor = min(min_xor, xor_sum);
             }
         }
 
+        // Store the minimum XOR sum for the current state (i, mask) in the dp array.
         dp[mask] = min_xor;
+
+        // Return the minimum XOR sum for the current state (i, mask).
         return min_xor;
     }
 };
